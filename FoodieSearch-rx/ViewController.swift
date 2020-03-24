0afa2e9cfd9bf2e1587f6a02b35ce398efe0cca4
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-24 18:31:22 +0000

    Initial Commit

---
+++ FoodieSearch-rx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieSearch-rx
+//
+//  Created by Christian Leovido on 24/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
