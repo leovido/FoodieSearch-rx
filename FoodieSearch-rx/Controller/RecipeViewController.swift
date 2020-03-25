50181f19d957458e9046c11e78c52b8d0c2498ef
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-25 15:33:26 +0000

    add ingredientSearchBar and ingredientsCollection view binding setup

--- FoodieSearch-rx/Controller/RecipeViewController.swift
+++ FoodieSearch-rx/Controller/RecipeViewController.swift
@@ -13,7 +13,13 @@ import RxCocoa
 class RecipeViewController: UIViewController {
 
     @IBOutlet weak var recipeSearchBar: UISearchBar!
+    @IBOutlet weak var ingredientSearchBar: UISearchBar!
+
     @IBOutlet weak var recipesTableView: UITableView!
+    @IBOutlet weak var ingredientCollectionView: UICollectionView!
+
+    var query: String = ""
+    var ingredientsQuery: BehaviorRelay<String> = BehaviorRelay(value: "")
 
     let disposeBag = DisposeBag()
     let viewModel: RecipeViewModel = RecipeViewModel()
@@ -24,13 +30,15 @@ class RecipeViewController: UIViewController {
         self.parent?.title = "Recipes"
 
         setupSearchBar()
+        setupIngredientSearchBar()
         setupBindings()
+        setupBindingCollectionView()
         setupErrors()
     }
 
     func setupErrors() {
 
-        viewModel.errorRecipesPublisher
+        viewModel.errorDataSourcePublisher
             .asObserver()
             .subscribe(onNext: { error in
                 
@@ -44,12 +52,32 @@ class RecipeViewController: UIViewController {
 
         recipeSearchBar.rx.text
             .orEmpty
-            .throttle(RxTimeInterval.milliseconds(1000), scheduler: MainScheduler.instance)
+            .throttle(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
             .distinctUntilChanged()
             .subscribe(onNext: { query in
-                self.viewModel.getRecipes(page: 1, ingredients: "", query: query)
+
+                self.query = query
+
+                self.viewModel.getRecipes(page: 1, ingredients: self.ingredientsQuery.value, query: query)
+
             }, onError: { error in
-                print(error)
+                self.showAlertError(error)
+            })
+        .disposed(by: disposeBag)
+
+    }
+
+    func setupIngredientSearchBar() {
+
+        ingredientSearchBar.rx.text
+            .orEmpty
+            .throttle(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { query in
+                self.viewModel.getRecipes(page: 1, ingredients: self.ingredientsQuery.value, query: self.query)
+            }, onError: { error in
+                self.showAlertError(error)
+
             })
         .disposed(by: disposeBag)
 
@@ -57,7 +85,7 @@ class RecipeViewController: UIViewController {
 
     func setupBindings() {
 
-        viewModel.recipes
+        viewModel.dataSource
         .asObservable()
             .bind(to: recipesTableView.rx.items(cellIdentifier: RecipeCell.identifier,
                                                 cellType: RecipeCell.self)) { _, element, cell in
@@ -65,6 +93,42 @@ class RecipeViewController: UIViewController {
                 cell.configureCell(recipe: element)
 
             }
+        .disposed(by: disposeBag)
+
+        ingredientsQuery
+            .bind(to: ingredientSearchBar.rx.text)
+        .disposed(by: disposeBag)
+
+    }
+
+    func setupBindingCollectionView() {
+
+        Observable.just(["Kale", "Tuna", "Eggs", "Bread", "Salmon"])
+            .asObservable()
+            .debug("ingredients", trimOutput: true)
+            .bind(to: ingredientCollectionView.rx.items(cellIdentifier: IngredientCell.identifier, cellType: IngredientCell.self)) { _, element, cell in
+
+                cell.configure(element: element)
+        }
+        .disposed(by: disposeBag)
+
+        ingredientCollectionView.rx
+            .modelSelected(String.self)
+            .subscribe(onNext: { ingredient in
+
+                let previous = self.ingredientsQuery.value
+                self.ingredientsQuery.accept(previous)
+
+                if previous.isEmpty {
+                    self.ingredientsQuery.accept(previous + ingredient)
+                } else {
+                    self.ingredientsQuery.accept(previous + ", " + ingredient)
+                }
+
+
+            }, onError: { error in
+                self.showAlertError(error)
+            })
         .disposed(by: disposeBag)
 
     }
